1cc48c099a849bc7f4fe0269c724eaaf16c86463
Solo <juan@xmartlabs.com>
2023-04-16 14:45:11 -0300

    base config within app root component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import NextProgress from "nextjs-progressbar";
 import "react-lazy-load-image-component/src/effects/opacity.css";
 import "src/styles/globals.css";
+import { PRIMARY_COLOR } from "src/utils/constants";
+import { trpc } from "src/utils/trpc";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+interface Props {
+  session: Session | null;
 }
 
-export default MyApp;
+function MyApp({ Component, pageProps }: AppProps<Props>) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <NextProgress color={PRIMARY_COLOR} options={{ showSpinner: false }} />
+      <Toaster />
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
+}
+
+export default trpc.withTRPC(MyApp);
