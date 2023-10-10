d59a295642e5a88ea538577c66b6b64581f82705
Solo <juan@xmartlabs.com>
2023-10-10 16:47:21 -0300

    app layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,3 +1,19 @@
-export default function AppLayout() {
-  return null;
+import React from 'react';
+
+import Header from 'src/components/Header';
+import Meta from 'src/components/Meta';
+import { meta } from 'src/utils/constants';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function AppLayout({ children }: Props) {
+  return (
+    <div className='text-white'>
+      <Meta title={meta.title} description={meta.description} />
+      <Header />
+      <div className='container mt-[57px] flex'>{children}</div>
+    </div>
+  );
 }
