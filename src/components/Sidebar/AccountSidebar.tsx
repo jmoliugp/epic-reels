fe72dc5738bc4c776fe38970b0346c8d8769e392
Solo <juan@xmartlabs.com>
2023-10-11 17:19:35 -0300

    followers account sidebar integration

--- src/components/Sidebar/AccountSidebar.tsx
+++ src/components/Sidebar/AccountSidebar.tsx
@@ -1,13 +1,36 @@
-import AccountSidebarItem from "src/components/Sidebar/AccountSidebarItem";
+import { useSession } from 'next-auth/react';
+import AccountSidebarItem from 'src/components/Sidebar/AccountSidebarItem';
+import { Account } from 'src/types';
+import { trpc } from 'src/utils/trpc';
 
-interface Props {}
+interface Props {
+  title: 'Suggested accounts' | 'Following accounts';
+  type: 'getAccountsSuggestion' | 'getAccountsFollowing';
+}
 
-export default function AccountSidebar({}: Props) {
+export default function AccountSidebar({ title, type }: Props) {
+  function getAccount() {
+    if (type === 'getAccountsFollowing' && session?.user) {
+      // TODO: perform getAccountFollowing.
+      return trpc.follow.getAccountsSuggestion.useQuery();
+    }
+
+    return trpc.follow.getAccountsSuggestion.useQuery();
+  }
+
+  const { data: session } = useSession();
+  const { data, isLoading } = getAccount();
+
+  if (isLoading) return null;
+  if (!session?.user && type === 'getAccountsFollowing') return null;
+
   return (
-    <div className="border-t border-[#484848] py-4">
-      <h3 className="mb-2 hidden px-2 text-sm font-medium lg:block">title</h3>
+    <div className='border-t border-[#484848] py-4'>
+      <h3 className='mb-2 hidden px-2 text-sm font-medium lg:block'>{title}</h3>
       <div>
-        <AccountSidebarItem />
+        {data?.accounts.map((item) => {
+          return <AccountSidebarItem key={item.id} account={item as Account} />;
+        })}
       </div>
     </div>
   );
