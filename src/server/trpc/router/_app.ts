08cc6d7d8f3283da3a3670095d239d7946444512
Solo <juan@xmartlabs.com>
2023-10-11 17:18:57 -0300

    follow router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,6 +1,9 @@
-import { router } from "src/server/trpc/trpc";
+import { followRouter } from 'src/server/trpc/router/follow';
+import { router } from 'src/server/trpc/trpc';
 
-export const appRouter = router({});
+export const appRouter = router({
+  follow: followRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- src/server/trpc/router/follow.ts
+++ src/server/trpc/router/follow.ts
@@ -1,3 +1,29 @@
-import { router } from "src/server/trpc/trpc";
+import { publicProcedure, router } from 'src/server/trpc/trpc';
 
-export const followRouter = router({});
+export const followRouter = router({
+  getAccountsSuggestion: publicProcedure.query(async ({ ctx }) => {
+    const accounts = await ctx.prisma.user.findMany({
+      where: {
+        id: {
+          not: ctx.session?.user?.id,
+        },
+      },
+      include: {
+        _count: {
+          select: {
+            followers: true,
+            followings: true,
+          },
+        },
+      },
+      orderBy: {
+        followers: {
+          _count: 'desc',
+        },
+      },
+      take: 10,
+    });
+
+    return { accounts };
+  }),
+});
