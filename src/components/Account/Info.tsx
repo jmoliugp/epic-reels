14cd68fc0b2153f8703934c22a25c5893cf6bda1
Solo <juan@xmartlabs.com>
2023-10-13 14:07:52 -0300

    display profile data

--- src/components/Account/Info.tsx
+++ src/components/Account/Info.tsx
@@ -1,37 +1,55 @@
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useSession } from 'next-auth/react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Profile } from 'src/types';
+import { getUsername } from 'src/utils';
 
-interface Props {}
+interface Props {
+  profile: Profile;
+  isFollow: boolean;
+}
 
-export default function Info({}: Props) {
+export default function Info({ isFollow, profile }: Props) {
+  const session = useSession().data;
+  const isOwnProfile = session?.user?.id === profile.id;
+
   return (
-    <div className="px-4 xl:px-0">
-      <div className="flex items-center">
-        <div className="h-[116px] w-[116px]">
+    <div className='px-4 xl:px-0'>
+      <div className='flex items-center'>
+        <div className='h-[116px] w-[116px]'>
           <LazyLoadImage
-            className="h-[116px] w-[116px] rounded-full"
-            effect="opacity"
+            className='h-[116px] w-[116px] rounded-full'
+            effect='opacity'
+            src={profile.image}
           />
         </div>
-        <div className="ml-5 flex-1">
-          <h1 className="text-[20px] font-semibold">@username</h1>
-          <p className="text-[16px] font-normal">name</p>
-          <button
-            className={`mt-4 rounded-md ${
-              true ? "bg-[#2f2f2f]" : "bg-primary"
-            } px-6 py-1 text-[16px] font-semibold`}
-          >
-            Following
-          </button>
+        <div className='ml-5 flex-1'>
+          <h1 className='text-[20px] font-semibold'>
+            @{getUsername(profile.name)}
+          </h1>
+          <p className='text-[16px] font-normal'>{profile.name}</p>
+          {!isOwnProfile && (
+            <button
+              className={`mt-4 rounded-md ${
+                true ? 'bg-[#2f2f2f]' : 'bg-primary'
+              } px-6 py-1 text-[16px] font-semibold`}
+            >
+              Following
+            </button>
+          )}
         </div>
       </div>
-      <div className="mt-5 flex items-center">
-        <div className="mr-5 flex items-center">
-          <p className="mr-2 text-[16px] font-semibold">0</p>
-          <p className="text-[16px] font-normal text-gray-300">Following</p>
+      <div className='mt-5 flex items-center'>
+        <div className='mr-5 flex items-center'>
+          <p className='mr-2 text-[16px] font-semibold'>
+            {profile._count.followings}
+          </p>
+          <p className='text-[16px] font-normal text-gray-300'>Following</p>
         </div>
-        <div className="flex items-center">
-          <p className="mr-2 text-[16px] font-semibold">0</p>
-          <p className="text-[16px] font-normal text-gray-300">Followers</p>
+        <div className='flex items-center'>
+          <p className='mr-2 text-[16px] font-semibold'>
+            {profile._count.followers}
+          </p>
+          <p className='text-[16px] font-normal text-gray-300'>Followers</p>
         </div>
       </div>
     </div>
