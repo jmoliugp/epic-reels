105dd5bcd4e5faaa94aa7bec0f051d8f23daa1a7
Solo <juan@xmartlabs.com>
2023-10-16 17:04:47 -0300

    create video mutation

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,10 @@
 import { followRouter } from 'src/server/trpc/router/follow';
+import { videoRouter } from 'src/server/trpc/router/video';
 import { router } from 'src/server/trpc/trpc';
 
 export const appRouter = router({
   follow: followRouter,
+  video: videoRouter,
 });
 
 // export type definition of API
--- src/server/trpc/router/video.ts
+++ src/server/trpc/router/video.ts
@@ -1,3 +1,27 @@
-import { router } from "src/server/trpc/trpc";
+import { protectedProcedure, router } from 'src/server/trpc/trpc';
+import { z } from 'zod';
 
-export const videoRouter = router({});
+export const videoRouter = router({
+  createVideo: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        videoWidth: z.number(),
+        videoHeight: z.number(),
+        videoUrl: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const video = await ctx.prisma.video.create({
+        data: {
+          height: input.videoHeight,
+          title: input.title,
+          videoUrl: input.videoUrl,
+          width: input.videoWidth,
+          userId: ctx.session.user.id,
+        },
+      });
+
+      return { video };
+    }),
+});
