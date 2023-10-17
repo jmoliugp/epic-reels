da6cb15b636ff9914b0c0d2c7ca67c09f1071536
Solo <juan@xmartlabs.com>
2023-10-17 13:17:19 -0300

    like router & like video mutation

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { followRouter } from 'src/server/trpc/router/follow';
+import { likeRouter } from 'src/server/trpc/router/like';
 import { videoRouter } from 'src/server/trpc/router/video';
 import { router } from 'src/server/trpc/trpc';
 
 export const appRouter = router({
   follow: followRouter,
   video: videoRouter,
+  like: likeRouter,
 });
 
 // export type definition of API
--- src/server/trpc/router/like.ts
+++ src/server/trpc/router/like.ts
@@ -1,3 +1,34 @@
-import { router } from "src/server/trpc/trpc";
+import { protectedProcedure, router } from 'src/server/trpc/trpc';
+import { z } from 'zod';
 
-export const likeRouter = router({});
+export const likeRouter = router({
+  likeVideo: protectedProcedure
+    .input(
+      z.object({
+        videoId: z.string().nullable(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const likes = await ctx.prisma.likes.findMany({
+        where: {
+          userId: ctx.session.user.id,
+          videoId: input.videoId,
+        },
+      });
+
+      if (likes.length > 0) {
+        return ctx.prisma.likes.delete({
+          where: {
+            id: likes[0]?.id,
+          },
+        });
+      } else {
+        return ctx.prisma.likes.create({
+          data: {
+            userId: ctx.session.user.id,
+            videoId: input.videoId!,
+          },
+        });
+      }
+    }),
+});
